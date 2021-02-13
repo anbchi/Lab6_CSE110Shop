130d832f8d6ff9b1a32736920c199a00e89d0975
anbchi <77029692+anbchi@users.noreply.github.com>
2021-02-13 11:07:44 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,17 @@
 // Script.js
-
+let list = document.getElementById("product-list");
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  let myFetch = fetch('https://fakestoreapi.com/products');
+  myFetch.then(response => response.json())
+    .then(function(data) {
+      localStorage.setItem("product-list", JSON.stringify(data));
+      for(i=0;i<data.length;i++){
+        let productItem = document.createElement('product-item');
+        productItem.imgSrc = data[i].image;
+        productItem.titleSrc = data[i].title;
+        productItem.priceSrc = data[i].price;
+        list.appendChild(productItem);
+      }
+    })
 });
